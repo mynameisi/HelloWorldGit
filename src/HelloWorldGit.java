d46de031f797fe4230663f5fda92634050acea09
mynameisi <test@nowhere.com>
2013-08-04 23:17:01 +0800

    项目初建

---
+++ src/HelloWorldGit.java
@@ -0,0 +1,20 @@
+/**
+ * 如果你能看到这段代码，表示你成功的在你的Eclipse中搭建好了EGit环境 <br />
+ * 并从我的EGit中pull【拉/下载】到了这个项目
+ * 下面按照main中的步骤把这个项目push【推/上传】到你的GitHubL里面吧
+ * 
+ * @author wangguozheng
+ * 
+ */
+public class HelloWorldGit {
+
+	public static void main(String[] args) {
+		// 1. 取消下面这句话的注释
+		// System.out.println("Hello World Git GitHub");
+
+		// 2. 在自己的GitHub中创建一个repo，取名为FirstProject
+		// 3. 把这个项目commit【保存修改】,然后push【推/上传】到你的FirstProject repo里面
+
+	}
+
+}
